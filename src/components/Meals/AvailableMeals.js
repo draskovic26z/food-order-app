2212ea95caa75eb3af115d29634bb4e43ae5285c
Nikola Drašković <draskovic26z@gmail.com>
2022-09-21 11:19:58 +0200

    Final changes

--- src/components/Meals/AvailableMeals.js
+++ src/components/Meals/AvailableMeals.js
@@ -6,25 +6,25 @@ const DUMMY_MEALS = [
 	{
 		id: "m1",
 		name: "Sushi",
-		description: "Finest fish and veggies",
+		description: "Maki or Nigiri",
 		price: 22.99,
 	},
 	{
 		id: "m2",
 		name: "Schnitzel",
-		description: "A german specialty!",
+		description: "A german specialty",
 		price: 16.5,
 	},
 	{
 		id: "m3",
 		name: "Barbecue Burger",
-		description: "American, raw, meaty",
+		description: "A classic American meal",
 		price: 12.99,
 	},
 	{
 		id: "m4",
 		name: "Green Bowl",
-		description: "Healthy...and green...",
+		description: "Healthy and green",
 		price: 18.99,
 	},
 ];
--- src/components/Meals/MealsSummary.js
+++ src/components/Meals/MealsSummary.js
@@ -4,12 +4,7 @@ const MealsSummary = () => {
 	return (
 		<section className={classes.summary}>
 			<h2>Delicious food, Delievered to you</h2>
-			<p>
-				BLAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA
-			</p>
-			<p>
-				BLAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA
-			</p>
+			<p>Enjoy our best meals!</p>
 		</section>
 	);
 };
